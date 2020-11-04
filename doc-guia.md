93d3ac32f5edd994f57408d4404ffd9b7e1beb20
alekgomes <lucas.maistro@gmail.com>
2020-11-04 16:23:38 -0300

    iniciando projeto

---
+++ doc-guia.md
@@ -0,0 +1,4 @@
+# Documento guia
+Documento para auxiliar resolução do teste, armazenar ideias, marcar milestone, etc
+
+[] Obter acesso a API do Spotfy
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return "Oi from React";
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
