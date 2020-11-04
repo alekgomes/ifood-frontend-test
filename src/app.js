ca6aa4b4c103123f20f1ee4115d1a865cb749ea4
alekgomes <lucas.maistro@gmail.com>
2020-11-04 20:37:42 -0300

    get spotify access token

--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 const App = () => {
+  const [spotifyAccessToken, setSpotifyAccessToken] = useState("");
+
+  useEffect(() => {
+    getSpotifyAccessToken().then(({ data }) =>
+      setSpotifyAccessToken(data.access_token)
+    );
+  }, []);
+
+  const getSpotifyAccessToken = () => {
+    const client_id = "1dc68d9df5d5462d86454ec32ba81774";
+    const client_secret = "04fc9360ff664605857ce385a4c4dea8";
+    const hash = btoa(`${client_id}:${client_secret}`);
+
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+
+    const config = {
+      method: "POST",
+      url: "https://accounts.spotify.com/api/token",
+      headers: {
+        Authorization: `Basic ${hash}`,
+      },
+      data: params,
+    };
+
+    return axios(config);
+  };
+
+  console.log(spotifyAccessToken);
   return "Oi from React";
 };
 
